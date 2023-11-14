66492a039a849bb2a77bf512551284a1dff18008
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2023-11-14 16:05:40 +0800

    Added url shortener api

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const { long, short } = defineProps(['long', 'short'])
+const emit = defineEmits()
+
+function copyToClipBoard() {
+  emit('copy')
+}
+
+function removeCard() {
+  emit('delete')
+}
+</script>
+<template>
+  <div>
+      {{ long }} {{ short }}
+      <button class="copy-btn" @click="copyToClipBoard">Copy to Clipboard</button>
+      <button class="remove-btn" @click="removeCard">Delete</button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,71 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import Navbar from '../components/Navbar.vue'
+import Card from '../components/Card.vue'
+// import Footer from '../components/Navbar.vue'
+import { ref } from 'vue'
+import Clipboard from 'clipboard'
+
+
+const originalLink = ref('')
+const links = ref([])
+
+
+function shortenLink() {
+  const url = `https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL?long=${originalLink.value}`
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    // body: JSON.stringify(data)
+  })
+  .then(res => {
+    return res.json()
+  })
+  .then(data => {
+    console.log(data)
+    links.value = [...links.value, {
+      'long': data.long,
+      'short': `https://1pt.co/${data.short}`
+    }]
+ })
+  .catch(err => {
+    console.error(err);
+  })
+
+}
+
+function copyToClipboard(shortLink) {
+  const clipboard = new Clipboard('.copy-btn', {
+    text: function () {
+      return shortLink;
+    }
+  });
+
+  clipboard.on('success', function (e) {
+    console.log('Copied to clipboard');
+    e.clearSelection();
+  });
+
+  clipboard.on('error', function (e) {
+    console.error('Unable to copy to clipboard');
+  });
+
+  clipboard.destroy();
+}
+
+function removeCard(itemId) {
+  // links.value = links.value.filter(link => link.id !== itemId);
+  console.log("hello")
+  console.log(itemId)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <input type="text" v-model="originalLink">
+    <button @click="shortenLink">Shorten</button>
+    <div v-for="link in links" :key="link.id">
+      <Card :long="link.long" :short="link.short" @copy="copyToClipboard(link.short)" @remove="removeCard(link.id)"/>
+    </div>
 </template>
